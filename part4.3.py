08012eca9f2019bdc57da33e1592b122c3f5b92f
stijnzwl <stijnwillems97@hotmail.com>
2025-04-25 12:46:41 +0200

    4.3 finished

--- part4.3.py
+++ part4.3.py
@@ -39,4 +39,37 @@
 #         break
 #     else:
 #         word_lst.append(word)
-        
+
+
+# lst = []
+
+# while True:
+#     num = int(input("New item: "))
+#     if num == 0:
+#         print("Bye!")
+#         break
+#     else:
+#         lst.append(num)
+#         print(lst)
+#         print(sorted(lst))
+
+
+# def length(lst):
+#     return len(lst)
+
+# result = length([1, 1, 1, 1])
+# print("The length is", result)    
+
+# def mean(int_lst):
+#     return sum(int_lst) / len(int_lst)  
+
+# my_list = [1, 2, 3, 4, 5]
+# result = mean(my_list)
+# print("mean value is", result)
+
+def range_of_list(int_lst):
+    return abs(max(int_lst) - min(int_lst))
+
+my_list = [1, 2, 3, 4, 5]
+result = range_of_list(my_list)
+print("The range of the list is", result)
