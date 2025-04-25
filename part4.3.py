aa95b9b425f8b25d9276ca3945999a7c5f0f3bc3
stijnzwl <stijnwillems97@hotmail.com>
2025-04-25 11:45:59 +0200

    lists

---
+++ part4.3.py
@@ -0,0 +1,11 @@
+# lst = [1, 2, 3, 4, 5]
+
+# # while True:
+# #     index = int(input())
+# #     if index == -1:
+# #         break
+# #     new_value = int(input())
+# #     lst[index] = new_value
+# #     print(lst)
+    
+    
