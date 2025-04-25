c2a7fa93e8001730c439c0613c6534fc16cf6cc6
stijnzwl <stijnwillems97@hotmail.com>
2025-04-25 12:10:21 +0200

    more lists

--- part4.3.py
+++ part4.3.py
@@ -1,5 +1,4 @@
 # lst = [1, 2, 3, 4, 5]
-
 # # while True:
 # #     index = int(input())
 # #     if index == -1:
@@ -9,3 +8,35 @@
 # #     print(lst)
     
     
+# lst = []
+# amount = int(input("How many items: "))
+# for x in range(1, amount + 1):
+#     lst.append(int(input(f"Item {x}: ")))  
+# print(lst)
+
+# lst = []
+# while True:
+#     print(f"The list is now {lst}")
+#     choice = str(input("a(d)d, (r)emove or e(x)it: "))
+#     if len(lst) > 0 and choice == 'd':
+#         lst.append(lst[-1] + 1)
+#     elif len(lst) <= 0 and choice == 'd':
+#         lst.append(1)
+#     elif len(lst) > 0 and choice == 'r':
+#         lst.pop(-1)
+#     elif len(lst) <= 0 and choice == 'r':
+#         continue
+#     if choice == 'x':
+#         print('Bye!')
+#         break
+
+
+# word_lst = []
+# while True:
+#     word = str(input("Word: "))
+#     if word in word_lst:
+#         print(f"You typed in {len(word_lst)} different words")
+#         break
+#     else:
+#         word_lst.append(word)
+        
